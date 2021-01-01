60c4a1eb9059d752d867921672079b5e96f4a828
Philip Ramon <philldeveloper@github.com>
2021-01-01 12:24:55 -0300

    01/01 after refact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router'; //estrategia de carregamento rápido dos modulos
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -48,7 +48,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
+    HttpClientModule,
     SharedModule.forRoot(), //Module com os providers dentro, não precisa do core.
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,52 +1,39 @@
-import {Injectable, Inject} from '@angular/core'
-import {Http} from '@angular/http'
+import {Injectable} from '@angular/core'
+import {HttpClient, HttpParams} from '@angular/common/http'
 
 import {Observable} from 'rxjs/Observable'
-
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import {Restaurant} from "./restaurant/restaurant.model"
 import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model'
 
-import{MEAT_API} from '../app.api'
+import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
-
 
 @Injectable()
 export class RestaurantsService {
 
-    constructor(private http: Http){}
+    constructor(private http: HttpClient){}
 
-    /* o paramento 'q:search' indica que basicamente qualquer atributo de restaurant
-    pode ser pesquisado, facilitando o campo de busca */
-
-    /* (search?:string) indica que esse metodo é opcional, logo qualquer função que esteja
-    chamando restaurants nao precisa obrigatoriamente passar uma string */
-    
-    restaurants(search?: string): Observable<Restaurant[]>{
-        return this.http.get(`${MEAT_API}/restaurants`, {params: {q: search}})
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+    restaurants(search?: string): Observable<Restaurant[]> {
+        let params: HttpParams = undefined
+        if(search){
+            params = new HttpParams().append('q', search)
+        }
+        return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`, {params: params})
     }
 
     restaurantById(id: string): Observable<Restaurant>{
-        return this.http.get(`${MEAT_API}/restaurants/${id}`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+      return this.http.get<Restaurant>(`${MEAT_API}/restaurants/${id}`)
     }
 
     reviewsOfRestaurant(id: string): Observable<any>{
-        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+      return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
     }
 
     menuOfRestaurant(id: string): Observable<MenuItem[]>{
-        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+      return this.http.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`)
     }
-
 
 }
